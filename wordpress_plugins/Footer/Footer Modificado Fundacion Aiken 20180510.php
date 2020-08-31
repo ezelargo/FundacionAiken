f23c5b9ddeff685a08bd383b7a524bb915778877
Ezequiel Esteban Gonzalez <ezelargo@gmail.com>
2020-08-31 12:18:52 -0300

    Archivos varios
    Fuentes
    Imagenes comunes
    Landing page de 10 años

---
+++ wordpress_plugins/Footer/Footer Modificado Fundacion Aiken 20180510.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Sydney
+ */
+?>
+			</div>
+		</div>
+	</div><!-- #content -->
+
+	<?php do_action('sydney_before_footer'); ?>
+
+	<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+		<?php get_sidebar('footer'); ?>
+	<?php endif; ?>
+
+    <a class="go-top"><i class="fa fa-angle-up"></i></a>
+		
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info container">
+			Creado con
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'sydney' ) ); ?>" target="_blank">
+				 WordPress 
+			</a>
+			por Fundación Aiken &copy; 2008 - <?php echo date('Y'); ?>
+			<span class="sep"> | </span>
+			Personería jurídica número 0002142.			
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+	<?php do_action('sydney_after_footer'); ?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
